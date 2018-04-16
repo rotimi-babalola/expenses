79080586d0279bfbdad2f6bf5828968c49561c70
rotimi-babalola <rotimi.babalola@andela.com>
2018-04-16 20:56:51 +0100

    express integration

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({
+    message: 'Hello there!',
+  })
+});
+
+app.listen(port, () => console.log('Magic on this port'));
