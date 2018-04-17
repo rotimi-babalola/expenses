683d8edfe3edeedfa91079d4c7e5d8019c6e6db0
rotimi-babalola <rotimi.babalola@andela.com>
2018-04-17 18:34:54 +0100

    commit before graphql js files

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
+var graphqlHTTP = require('express-graphql');
+var { graphql, buildSchema } = require('graphql');
 import express from 'express';
-import router from './routes';
-import { graphQLRouter } from './routes/graphQLRouter';
-import connect from './db';
-import appConfig from './config';
-import setUpMiddWare from './middleware';
-
 const app = express();
 
-// middlewares mounted
-setUpMiddWare(app);
-// connect to DB
-connect();
+const port = 3000;
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    abiodun: String
+  }
+`);
 
-app.use(router);
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  abiodun: () => {
+    return 'abidoun';
+  },
+};
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(3000);
+// Construct a schema, using GraphQL schema language
 
-/* eslint no-undef:0 */
-const port = appConfig.port || 3000;
 
-app.listen(port, () => console.log(`App running on port ${port}`));
-export default app;
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, 'query { hello }', root).then((response) => {
+  console.log(response);
+});
+
+// import router from './routes';
+// import { graphQLRouter } from './routes/graphQLRouter';
+// import connect from './db';
+// import appConfig from './config';
+// import setUpMiddWare from './middleware';
+
+
+// // middlewares mounted
+// setUpMiddWare(app);
+// // connect to DB
+// connect();
+
+// app.use(router);
+
+// /* eslint no-undef:0 */
+
