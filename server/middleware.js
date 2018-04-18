732db337b542ddbd987b5a8fe509cdca0bc79974
rotimi-babalola <rotimi.babalola@andela.com>
2018-04-18 12:05:30 +0100

    add whitelist to cors

--- server/middleware.js
+++ server/middleware.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const setGlobalMiddleware = (app) => {
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false, }));
-  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors(corsOptions));
 };
 
 export default setGlobalMiddleware;
