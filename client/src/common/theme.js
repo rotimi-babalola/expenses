607c80e87a4b84012d071ac22c61b9902fc095f8
oreoluwade <oreoluwade@gmail.com>
2018-04-18 13:10:31 +0100

    create custom button

---
+++ client/src/common/theme.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: '#4CAF50',
+    secondary: '#C13A38',
+  },
+  greys: {
+    darkest: '#000000',
+    dark: '#495664',
+    light: '#7B8897',
+    lighter: '#E5E9F2',
+    lightest: '#ffffff',
+  },
+};
