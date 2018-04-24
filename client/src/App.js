510b176ec20b4614c96dcf66b404589c14be9108
rotimi-babalola <rotimi.babalola@andela.com>
2018-04-24 14:54:02 +0100

    first iteration of layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from './components';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gastos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Sample</Button>
+        <Button style={{ marginTop: '10px' }}>Sample</Button>
       </div>
     );
   }
